2091fe78a9f46e6f47eef1147ef59aa1eb35e68f
pkindalov <panayotkindalov@gmail.com>
2021-06-05 00:27:37 +0300

    Added result and test files. Other small changes

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 require_once('SimpleResizer.php');
 require_once('SimpleArchiver.php');
 
-$small = new SimpleResizer('input/', 'output/', '815x614', 72);
-$small->resizeAll();
+    $small = new SimpleResizer('input/', 'output/', '815x614', 300, 0);
+    $small->resizeAll();
 
-$medium = new SimpleResizer('input/', 'output/', '1630x1227', 300);
-$medium->resizeAll();
+    $medium = new SimpleResizer('input/', 'output/', '1630x1227', 300, 0);
+    $medium->resizeAll();
 
-$large = new SimpleResizer('input/', 'output/', '2751x2072', 300);
-$large->resizeAll();
+    $large = new SimpleResizer('input/', 'output/', '2751x2072', 300, 0);
+    $large->resizeAll();
 
-$xLarge = new SimpleResizer('input/', 'output/', '4256x2832', 300);
-$xLarge->resizeAll();
+    $xLarge = new SimpleResizer('input/', 'output/', '4256x2832', 300, 0);
+    $xLarge->resizeAll();
 
 $archiver = new SimpleArchiver('output/', 'output/', 'bundle');
 $archiver->zip();
